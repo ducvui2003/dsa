06d32b87e9124e9b8386f4ab3c555e28571a9bba
Le Anh Duc <ducvui2003@gmail.com>
2026-01-10 22:48:36 +0700

    3 sum use 2 pointers

--- leet_code/_15/Solution.java
+++ leet_code/_15/Solution.java
@@ -2,71 +2,50 @@ package leet_code._15;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 /*
 * https://leetcode.com/problems/3sum/description/
 * */
-import java.util.Map;
 
 public class Solution {
-//    public List<List<Integer>> threeSum(int[] nums) {
-//         if(nums.length == 3)
-//             return List.of(List.of(1,2,3));
-//         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-//         for (int i = 0; i < nums.length; i++) {
-//             Integer remain = 
-//         }
-//     }
 
-      // brute force
-    public List<List<Integer>> twoSumHint1(int[] nums, int target) {
-        if(nums.length == 3)
-            return List.of(List.of(1,2,3));
-        List<List<Integer>>  results = new ArrayList<>();
+    // brute force
+    public List<List<Integer>> thrreeSumHint1(int[] nums) {
+        if (nums.length == 3 && nums[0] + nums[1] + nums[2] == 0)
+            return List.of(List.of(nums[0], nums[1], nums[2]));
+        List<List<Integer>> results = new ArrayList<>();
+        Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length && j!=i ; i++) {
-               for (int k = 0; k < nums.length&& k!=j && k!=i; k++) {
-                    if (nums[j] + nums[k] + nums[i] == 0) {
-                        results.add(List.of(nums[i], nums[j], nums[k]));
+            // Skip duplicate
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            // a + b +c == 0 => at least a < 0 || b < 0 || c < 0
+            if (nums[i] > 0)
+                break;
+
+            // i < l < r
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[l] + nums[i] + nums[r];
+                if (sum == 0) {
+                    results.add(List.of(nums[l], nums[i], nums[r]));
+
+                    // Skip duplicate
+                    while (l < r && nums[l] == nums[l + 1])
+                        l++;
+                    while (l < r && nums[r] == nums[r - 1])
+                        r--;
+                    l++;
+                    r--;
+                } else if (sum > 0) {
+                    r--;
+                } else {
+                    l++;
                 }
-               }
+
             }
         }
         return results;
     }
-
-     public int[] twoSumHint3(int[] nums, int target) {
-        if (nums.length == 2) {
-            return new int[]{
-                    0, 1
-            };
-        }
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int j = 0; j < nums.length; j++) {
-            Integer value = map.get(nums[j]);
-            if ((value == null))
-                map.put(nums[j], j);
-            else if (nums[j] * 2 == target)
-            // check if 2 index has same value
-                return new int[]{
-                        Math.min(j, value), Math.max(j, value)
-                };
-        }
-
-        for (int j = 0; j < nums.length; j++) {
-            int remain = target - nums[j];
-            Integer index = map.get(remain);
-            if (index != null) {
-                if (index == j)
-                    continue;
-                return new int[]{
-                        Math.min(j, index), Math.max(j, index)
-                };
-            }
-        }
-        return null;
-    }
-
 }
